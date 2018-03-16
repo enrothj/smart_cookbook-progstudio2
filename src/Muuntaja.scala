ed7fcdcbe10cadf510b3b5b61d313d2d91ef8d7f
janie <janie@DESKTOP-MMBEG8D>
2018-03-16 10:48:02 +0200

    Luotiin suhde-metodien tyngät ja alustavat kuvaukset

--- src/Muuntaja.scala
+++ src/Muuntaja.scala
@@ -2,4 +2,23 @@ package Smart_Cookbook
 
 object Muuntaja {
   
+  /*
+   * suhde-metodit ottavat parametreinaan kaksi mittayksikköä, ja palauttavat niiden suhteen.
+   */
+  
+  // suhdeMassa laskee kahden massayksikön suhteen (esim kg / g = 1000)
+  
+  def suhdeMassa(a: String, b: String): Double = {
+    ???
+  }
+  
+  // suhdeTilavuus laskee kahden tilavuuden yksikön suhteen (esim. l / dl = 10)
+  
+  def suhdeTilavuus(a: String, b: String): Double = {
+    ???
+  }
+  
+  def suhde = ???
+  
+  
 }
