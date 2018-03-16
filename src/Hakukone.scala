218c058b9d40ccf884cba4e1c40a61f91b4b0ea5
janie <janie@DESKTOP-MMBEG8D>
2018-03-16 11:04:09 +0200

    Luotiin metodien hae, voiValmistaa ja onOlemassa tyngät
    ja niiden alustavat kuvaukset.

--- src/Hakukone.scala
+++ src/Hakukone.scala
@@ -1,5 +1,27 @@
 package Smart_Cookbook
 
+
+/*
+ * 
+ */
+
 object Hakukone {
+  
+  /*
+   * Metodi hae käy läpi Varaston listan Aine-olioista ja palauttaa listan niistä, jotka täyttävät
+   * hakuehdot. Parametri n määrittelee kuinka monta ainetta saa puuttua halutuista resepteistä.
+   */
+  def hae(n: Int): Vector[Aine] = ???
+  
+  /*
+   * Metodi voiValmistaa ottaa parametrina Aine-olion ja tarkistaa riittävätkö varastossa olevat aineet
+   * halutun aineen valmistamiseen.
+   */
+  def voiValmistaa(aine: Aine): Boolean = ???
+  
+  /*
+   * Metodi onOlemassa tarkistaa onko ohjelmaan tallennettu parametrina annetun niminen aine.
+   */
+  def onOlemassa(nimi: String): Boolean = ???
   
 }
