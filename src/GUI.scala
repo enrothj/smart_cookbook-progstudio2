d0a6838c2dc9604459e6f21c0bbfc72d75322bdc
janie <janie@DESKTOP-MMBEG8D>
2018-04-23 16:42:06 +0300

    Lisättiin pääikkuna ja sen napit

--- src/GUI.scala
+++ src/GUI.scala
@@ -1,11 +1,35 @@
-package Smart_Cookbook
-
 import scala.swing._
-import java.awt._
+
 
 
 
 object GUI extends SimpleSwingApplication {
+  
+  def top = pääikkuna
+  
+  /*
+   * Pääikkunassa on kolme nappia: Reseptihaku, Luo Resepti ja Varaston hallinta. Näitä painamalla avataan ikkunoita, joilla voi toteuttaa ko. toimintoja.
+   * Nappien alla on lista ohjelman aineista, niiden määrästä varastossa ja niiden allergeeneista.
+   */
+  
+  // Pääkomponentit:
+  
+  val reseptihaku = new Button("Reseptihaku")
+  val luoResepti  = new Button("Luo Resepti")
+  val varHallinta = new Button("Varaston hallinta")
+  
+     // TODO: lista ohjelman aineista jne.
+  
+  // Komponenttien asemointi
+  val napit = new BoxPanel(Orientation.Horizontal)
+  napit.contents += reseptihaku
+  napit.contents += luoResepti
+  napit.contents += varHallinta
+  
+  val pääikkuna = new MainFrame
+  pääikkuna.contents = napit
+  pääikkuna.title    = "Älykäs reseptikirja"
+  pääikkuna.size     = new Dimension(400, 400)
   
   
   
