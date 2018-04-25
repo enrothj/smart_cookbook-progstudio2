3c5c87727a1e057a8a1a8870918d8f36aa84110a
janie <janie@DESKTOP-MMBEG8D>
2018-04-25 14:32:03 +0300

    Lisättiin poikkeusluokka KappaleMuunnos

--- src/Poikkeukset.scala
+++ src/Poikkeukset.scala
@@ -6,3 +6,5 @@ case class VirheellinenData(kuvaus: String, virheData: String) extends Exception
 
 case class VirheellinenMittayksikkö(kuvaus: String, virheData: String) extends Exception(kuvaus) {}
 
+// Tämä poikkeus tapahtuu, kun yritetään kutsua Muuntajan muunnosmetodeja kappaleille (mittayksikkö = "kpl")
+case class KappaleMuunnos(kuvaus: String = "Yritettiin muuntaa kappalemuotoista ainetta", virheData: String) extends Exception(kuvaus) {}
