32905379e3bfafcd2d64fbd458e9dc016662410f
janie <janie@DESKTOP-MMBEG8D>
2018-04-25 14:34:17 +0300

    Poistettiin oletuskonstruktori luokasta KappaleMuunnos

--- src/Poikkeukset.scala
+++ src/Poikkeukset.scala
@@ -7,4 +7,4 @@ case class VirheellinenData(kuvaus: String, virheData: String) extends Exception
 case class VirheellinenMittayksikkö(kuvaus: String, virheData: String) extends Exception(kuvaus) {}
 
 // Tämä poikkeus tapahtuu, kun yritetään kutsua Muuntajan muunnosmetodeja kappaleille (mittayksikkö = "kpl")
-case class KappaleMuunnos(kuvaus: String = "Yritettiin muuntaa kappalemuotoista ainetta", virheData: String) extends Exception(kuvaus) {}
+case class KappaleMuunnos(kuvaus: String, virheData: String) extends Exception(kuvaus) {}
