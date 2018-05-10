bc4e3c821db4562e0fe5e5e8d728e9418681f080
janie <janie@82.130.18.60>
2018-05-10 18:13:12 +0300

    Lisättiin poikkeusluokka OlematonAinesosa

--- src/Smart_Cookbook/Poikkeukset.scala
+++ src/Smart_Cookbook/Poikkeukset.scala
@@ -9,3 +9,6 @@ case class KappaleMuunnos(kuvaus: String, virheData: String) extends Exception(k
 
 // Tämä poikkeus tapahtuu, kun yritetään kutsua Aine-oliota, joka ei ole olemassa ohjelman muistissa.
 case class OlematonAinePoikkeus(kuvaus: String, virheData: String) extends Exception(kuvaus) {}
+
+// Tämä poikkeus tapahtuu, kun yritetään käsitellä ainesosaa, joka ei ole (enää) ohjelman tiedossa.
+case class OlematonAinesosa(kuvaus: String, virheData: String) extends Exception(kuvaus)
